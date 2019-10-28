3132673f7e064676979b2e52aeda39acc34d2c2b
Jiří Klepl <jiriklepl@seznam.cz>
2019-10-28 18:08:20 +0100

    fixed shell-common.c

--- mysh/src/shell-common.c
+++ mysh/src/shell-common.c
@@ -1,28 +1,19 @@
-#include "shell.h"
+#include "shell-common.h"
 
-void value_t_free(value_t *value) {
-    if (value->_type == str_t) {
-        free(value->_value.str);
+void value_t_free(value_t value) {
+    if (value._type == str_t) {
+        free(value._value.str);
     }
 }
 
-value_t value_t_assign(enum _type_t type, union _value_t value) {
-    value_t tmp;
-    switch (type) {
-        case str_t:
-            tmp._type = type;
-            tmp._value.str = malloc(sizeof(union _value_t));
+value_t value_t_str(const char* value) {
+    value_t tmp = {
+        ._type = str_t,
+        ._value.str = malloc(strlen(value))
+    };
 
-            if (tmp._value.str != NULL) {
-                strcpy(tmp._value.str, value.str);
-            }
-
-            break;
-        case error_t:
-        default:
-            tmp._type = error_t;
-
-            break;
+    if (tmp._value.str != NULL) {
+        strcpy(tmp._value.str, value);
     }
 
     return tmp;
